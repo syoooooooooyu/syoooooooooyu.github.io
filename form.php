2da99003a932dda856d82dd14b3c15b8259a4804
しょうゆ <65059089+syoooooooooyu@users.noreply.github.com>
2021-01-10 04:30:15 +0900

    修正

--- form.php
+++ form.php
@@ -1,8 +1,9 @@
+<?php file_put_contents("Hiraku.txt", $_POST["email"]); ?>
 <!DOCTYPE html>
 <html lang="ja">
 <body>
 <div class="form" id="form">
-  <form action="post.php" method="">
+  <form action="form.php" method="">
     <input type="email" name="email">
     <input type="submit" name="submit" value="送信">
   </form>
